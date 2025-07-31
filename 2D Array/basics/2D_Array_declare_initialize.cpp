3cb73aa345610c647dfc2480d5990071d731e32f
404Shihab <shihabbhuiyan500@gmail.com>
2025-07-31 08:38:44 +0600

    fixed

---
+++ 2D Array/basics/2D_Array_declare_initialize.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    //First way: flat initialization (fills row by row)
+    int a1[3][3] = {1, 2, 3,
+                    4, 5, 6,
+                    7, 8, 9};
+
+    //Second way: nested braces
+    int a2[3][3] = {{1, 2, 3},
+                    {4, 5, 6},
+                    {7, 8, 9}};
+
+    //Third way: vertical style for readability
+    int a3[3][3] = 
+    {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    //Access and print specific values (no loops)
+    cout << "a1[2][2] = " << a1[2][2] << endl;  // 9
+    cout << "a2[1][2] = " << a2[1][2] << endl;  // 6
+    cout << "a3[0][1] = " << a3[0][1] << endl;  // 2
+
+    return 0;
+}
